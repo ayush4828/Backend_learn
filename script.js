1bcedf09e5c887d4d1ffd4505df6d4e11d25853e
ayush4828 <ayushpatel11486@gmail.com>
2025-12-29 22:13:11 +0530

    learning require and module.export

---
+++ script.js
@@ -0,0 +1,27 @@
+// let n = 5;
+
+// for(let i = 0 ; i<=n; i++)
+// {
+//     console.log("hello," , i);
+// }
+
+// console.log("hello");
+
+
+
+
+// let arg = process.argv;
+
+// for(let i = 2 ; i<arg.length ; i++){
+// console.log("hello to " ,  arg[i]);
+// }
+
+
+const val = require("./math");
+
+console.log(val.sum(1,2));
+console.log(val.sub(5,2));
+console.log(val.div(9,3));
+console.log(val.mul(12,6));
+console.log(val.g);
+console.log(val.PI);
