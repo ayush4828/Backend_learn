02c28451247aad869400ecce3c3c7c4dc55e6582
ayush4828 <ayushpatel11486@gmail.com>
2025-12-30 20:08:29 +0530

    initial

---
+++ script.js
@@ -0,0 +1,37 @@
+// let n = 5;
+
+// for(let i = 0 ; i<=n; i++)
+// {
+//     console.log("hello," , i);
+// }
+
+// console.log("hello");
+
+
+
+
+// let arg = process.argv;
+
+// for(let i = 2 ; i<arg.length ; i++){
+// console.log("hello to " ,  arg[i]);
+// }
+
+
+// const val = require("./math");
+
+// console.log(val.sum(1,2));
+// console.log(val.sub(5,2));
+// console.log(val.div(9,3));
+// console.log(val.mul(12,6));
+// console.log(val.g);
+// console.log(val.PI);
+
+// const fal = require("./Fruits");
+
+// console.log(fal);
+
+// console.log(fal[0].name);
+
+import {sum} from "./math.js"
+
+console.log(sum(1,2));
