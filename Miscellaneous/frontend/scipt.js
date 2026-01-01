82c8ddec8e0ed5706b5a36036123353cc1ba7b94
ayush4828 <ayushpatel11486@gmail.com>
2026-01-02 00:33:49 +0530

    learning consepts of inheritance

--- Miscellaneous/frontend/scipt.js
+++ Miscellaneous/frontend/scipt.js
@@ -50,6 +50,25 @@
 /* this is the class  implementation   */
 
 
+// class person {
+//    constructor(name , age){
+//     this.name = name;
+//     this.age = age;
+//    }
+
+//     talk(){
+// console.log(console.log(`hello ${this.name}`))
+//    }
+
+// }
+
+// let p1 = new person("ayush" , 19);
+// let p2 = new person("vinit" , 19);
+
+
+
+/*  consepts of inheritance  */
+
 class person {
    constructor(name , age){
     this.name = name;
@@ -62,5 +81,19 @@ console.log(console.log(`hello ${this.name}`))
 
 }
 
-let p1 = new person("ayush" , 19);
-let p2 = new person("vinit" , 19);
+class student extends person{
+   constructor(name , age , marks){
+      super(name , age );
+      this.marks = marks;
+      }
+}
+
+class teacher extends person{
+   constructor(name , age , subject){
+      super(name , age );
+      this.subject = subject;
+      }
+}
+
+let s1 = new student("ayush" , 19 , 99);
+let t1 = new teacher("priyanka" , 36 , "AI/ML");
