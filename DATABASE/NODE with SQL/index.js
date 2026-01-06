c2e13205c67b9676f0b61ffd0a7cb8f7a46bb2a1
ayush4828 <ayushpatel11486@gmail.com>
2026-01-06 22:05:01 +0530

    mysql with node

--- DATABASE/NODE with SQL/index.js
+++ DATABASE/NODE with SQL/index.js
@@ -1,15 +1,34 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require("mysql2");
 
-let getRandomuser = ()=>{
-     return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
-  };
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'ayush',
+  password: 'Ayush@19102005'
+});
+
+
+try{
+  connection.query("SHOW TABLES" , (err,result)=>{
+if(err) throw err;
+  console.log(result);
+});
+}
+catch(err) {
+  console.log(err);
 }
 
-console.log(getRandomuser());
+connection.end();
+// let getRandomuser = ()=>{
+//      return {
+//     id: faker.string.uuid(),
+//     username: faker.internet.username(),
+//     email: faker.internet.email(),
+//     password: faker.internet.password(),
+  
+//   };
+// }
+
+// console.log(getRandomuser());
