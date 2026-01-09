47b63a773e8e19d0ebda81b2d4b2db8d9557335b
ayush4828 <ayushpatel11486@gmail.com>
2026-01-10 00:47:11 +0530

    inserting multiple data at a same time in collection

--- DATABASE/MONGO/index.js
+++ DATABASE/MONGO/index.js
@@ -21,11 +21,27 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User",userSchema);
-const Employee = mongoose.model("Employee",userSchema);
+// const Employee = mongoose.model("Employee",userSchema);
 
 //inserting single user data at a time
 
-const user1 = new User({name:"Ayush" , email : "ayush@gmnail.com" , age:20 });
-const user2 = new User({name:"vinit" , email : "vinit@gmnail.com" , age:21 });
-user1.save();
-user2.save();
+// const user1 = new User({name:"Ayush" , email : "ayush@gmnail.com" , age:20 });
+// const user2 = new User({name:"vinit" , email : "vinit@gmnail.com" , age:21 });
+// user1.save().then((res)=>{console.log(res)}).catch((err)=>{
+//     console.log(err)
+// });
+// user2.save().then((res)=>{console.log(res)}).catch((err)=>{
+//     console.log(err)
+// });
+
+
+
+//inserting multiple data at a time
+
+User.insertMany([
+{name:"jainik" , email:"jainik@gmail.com" , age : 20 },
+{name:"divy" , email:"divy@gmail.com" , age : 21},
+{name:"pratham" , email:"pratham@gmail.com" , age : 19}
+])
+.then(res=>{console.log(res)}).catch((err)=>{console.log(err)});
+
