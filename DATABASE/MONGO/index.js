04a210b1d331b756d00a4f8000050f09ae6fb026
ayush4828 <ayushpatel11486@gmail.com>
2026-01-09 21:38:28 +0530

    making connection with database

---
+++ DATABASE/MONGO/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+
+
+main().then(()=>{
+    console.log("success");
+})
+.catch(err=>console.log(err));
+
+async function main() {
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+
+}
