0e966524328e133cb6f2ee34d3a990fd5868a3f5
ayush4828 <ayushpatel11486@gmail.com>
2026-01-18 20:59:13 +0530

    learning about logger  for middlewares

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -3,21 +3,38 @@ const app = express();
 
 
 
-app.use((req,res,next)=>{
+// app.use((req,res,next)=>{
    
-   console.log(`on the middleware 1`);
+//    console.log("this is middleware 1");
+//    next();
+// //    return next(); // we can also write this for returning next()
+// //    console.log("after next()") possible but not prefer
+// })
+
+
+// app.use((req,res,next)=>{
+   
+//    console.log(`on the middleware 2`);
+//    next();
+
+// })
+
+//utility middleware // logger
+
+app.use((req,res,next)=>{
+   req.time =new Date(Date.now()).toString();
+   console.log(req.method , req.hostname , req.path , req.time);
    next();
-//    return next(); // we can also write this for returning next()
-//    console.log("after next()") possible but not prefer
 })
 
 
-app.use((req,res,next)=>{
-   
-   console.log(`on the middleware 2`);
-   next();
 
+app.use( "/random" , (req,res,next)=>{
+  
+   console.log("on the route random");
+   next();
 })
+
 app.get("/" , (req,res)=>{
     res.send("on root /")
 })
@@ -25,6 +42,12 @@ app.get("/" , (req,res)=>{
 app.get("/random" , (req,res)=>{
     res.send("on route random")
 })
+
+// app.use((req,res,next)=>{
+//    req.time =new Date(Date.now()).toString();
+//    console.log(req.method , req.hostname , req.path , req.time);
+//    next();
+// })  if we write after requested path then this will not print on  the console
 
 app.listen(8080 , ()=>{
     console.log("server start success")
